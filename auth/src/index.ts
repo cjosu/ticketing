49ca8bc98bc63eb925df8d93e88313f624337579
Colin O'Sullivan <88686663+cjosu@users.noreply.github.com>
2023-06-03 14:05:04 -0400

    JSON formatting + signin route handling

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -34,6 +34,10 @@ app.use(errorHandler);
 import { NotFoundError } from './errors/not-found-error';
 
 const start = async () => {
+  if (!process.env.JWT_KEY) {
+    throw new Error('JWT_KEY must be defined');
+  }
+
   try {
     await mongoose.connect('mongodb://auth-mongo-srv:27017/auth');
     console.log('Connected to MongoDb!');
