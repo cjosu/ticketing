57cefa7888fb0f1f5e3030fc1cff82ebb1915745
Colin O'Sullivan <88686663+cjosu@users.noreply.github.com>
2023-07-30 15:53:06 -0400

    input test

--- auth/src/errors/request-validation-error.ts
+++ auth/src/errors/request-validation-error.ts
@@ -3,14 +3,20 @@ import { CustomError } from './custom-error';
 
 export class RequestValidationError extends CustomError {
   statusCode = 400;
+
   constructor(public errors: ValidationError[]) {
     super('Invalid request parameters');
 
+    // Only because we are extending a built in class
     Object.setPrototypeOf(this, RequestValidationError.prototype);
   }
+
   serializeErrors() {
-    return this.errors.map(error => {
-      return { message: error.msg, field: error.param };
+    return this.errors.map(err => {
+      if (err.type === 'field') {
+        return { message: err.msg, field: err.path };
+      }
+      return { message: err.msg };
     });
   }
 }
